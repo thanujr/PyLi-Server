e115dfbb9240719dc50865202f3141992aeb6238
Thanuj <ravindranath.thanuj@gmail.com>
2015-06-28 14:33:59 +0530

    Refactored server code.

--- PyliServer.py
+++ PyliServer.py
@@ -5,75 +5,80 @@ import ConfigParser
 
 import RequestHandler
 
+class Server:
 
-def parseConfigs(configFile):
-    configMap = {}
-    config = ConfigParser.ConfigParser()
-    config.read(configFile)
-    configMap.update({'webdir':config.get('Webdirectory', 'directory')})
-    configMap.update({'host':config.get('Host', 'hostname')})
-    configMap.update({'port':config.get('Port', 'listenport')})
-    return configMap
+    def __init__(self):
+        self.configs = self.parseConfigs('pyli.conf')
 
-
-def createServerSocket():
-    listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    return listenSocket
-
-def handleRequest(clientSocket, configs):
-    request = clientSocket.recv(1024)
-    try:
-        getLine = request.splitlines()[0]
-        (method, path, version) = getLine.split()
-
-        rqstHandle = RequestHandler.RequestHandler(configs)
-
-        if(method == 'GET'):
-            response = rqstHandle.handleGETMethod(path)
-
-        if(method == 'HEAD'):
-            response = rqstHandle.handleHEADMethod(path)
-
-        if(method == 'POST'):
-            response = rqstHandle.handlePOSTMethod(request)
-
-        clientSocket.sendall(response)
-
-        clientSocket.close()
-
-    except:
-        clientSocket.close()
+    def parseConfigs(self, configFile):
+        configMap = {}
+        config = ConfigParser.ConfigParser()
+        config.read(configFile)
+        configMap.update({'webdir':config.get('Webdirectory', 'directory')})
+        configMap.update({'host':config.get('Host', 'hostname')})
+        configMap.update({'port':config.get('Port', 'listenport')})
+        return configMap
 
 
+    def createServerSocket(self):
+        listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-def serverStart(listenSocket, configs):
-    hostname = configs['host']
-    port = int(configs['port'])
+        return listenSocket
 
-    listenSocket.bind((hostname, port))
-    listenSocket.listen(5)
+    def handleRequest(self, clientSocket):
+        request = clientSocket.recv(1024)
+        try:
+            getLine = request.splitlines()[0]
+            (method, path, version) = getLine.split()
 
-    print "Server started at %s : %s" % (hostname, port)
+            rqstHandle = RequestHandler.RequestHandler(self.configs)
 
-    while True:
-        clientSocket, clientAddress = listenSocket.accept()
+            if(method == 'GET'):
+                response = rqstHandle.handleGETMethod(path)
 
-        pid = os.fork()
-        if pid == 0:
-            listenSocket.close()
-            handleRequest(clientSocket, configs)
-            os._exit(0)
-        else:
+            if(method == 'HEAD'):
+                response = rqstHandle.handleHEADMethod(path)
+
+            if(method == 'POST'):
+                response = rqstHandle.handlePOSTMethod(request)
+
+            clientSocket.sendall(response)
+
+            clientSocket.close()
+
+        except:
             clientSocket.close()
 
 
-def main():
-    serverSocket = createServerSocket()
-    configs = parseConfigs('pyli.conf')
 
-    serverStart(serverSocket, configs)
+    def serverStart(self, listenSocket):
+        hostname = self.configs['host']
+        port = int(self.configs['port'])
+
+        listenSocket.bind((hostname, port))
+        listenSocket.listen(5)
+
+        print "Server started at %s : %s" % (hostname, port)
+
+        while True:
+            clientSocket, clientAddress = listenSocket.accept()
+
+            pid = os.fork()
+            if pid == 0:
+                listenSocket.close()
+                self.handleRequest(clientSocket)
+                os._exit(0)
+            else:
+                clientSocket.close()
+
+
+
+### Start the main program.
+def main():
+    server = Server()
+    serverSocket = server.createServerSocket()
+    server.serverStart(serverSocket)
 
 
 if __name__ == '__main__':
